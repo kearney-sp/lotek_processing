15e2dfef6a874b30ccbc34c77b2efe3a13af7b78
kearney-sp <sean.durango@gmail.com>
2022-12-29 14:13:06 -0800

    backup from ARS_old after pull

--- lotek/gps.py
+++ lotek/gps.py
@@ -41,3 +41,48 @@ def calc_dist(start_coords, end_coords):
     """
     dist_tmp = np.linalg.norm(start_coords - end_coords, axis=1)
     return dist_tmp
+
+
+def make_steps(x, start_field_x='UTM_X_fnl_lag1', start_field_y='UTM_Y_fnl_lag1',
+               end_field_x='UTM_X_fnl', end_field_y='UTM_Y_fnl'):
+    from shapely.geometry import Point, LineString
+    """
+    Create line-strings (i.e., polylines) between each GPS start/end
+    ***NOTE: intended to be used with pandas.DataFrame.apply()***
+    
+    Parameters
+    -----------
+    start_field_x :      (string) name of the field with starting x coordinates
+    start_field_y :      (string) name of the field with starting y coordinates
+    end_field_x :      (string) name of the field with ending x coordinates
+    end_field_y :      (string) name of the field with ending y coordinates
+    
+    Returns
+    -----------
+    LineString() object from shapely module
+    """
+    line_step = LineString([Point(x[start_field_x], x[start_field_y]),
+                 Point(x[end_field_x], x[end_field_y])])
+    return line_step
+
+
+def graze_intensity(x):
+    """
+    Calculate average grazing intensity for a line segment connecting two GPS locations
+    based on the duration ('Fix_Duration') of time and the distance in meters ('steplength') 
+    between the two fixes
+    ***NOTE: intended to be used with geopandas.GeoDataFrame.apply()***
+    
+    Returns
+    ---------
+    A float value representing the seconds spent grazing per meter along the line
+    """
+    
+    if x['steplength'] == 0 or x['cell_ct'] == 0:
+        return 60.0*x['Fix_Duration']
+    else:
+        gi = (60.0*x['Fix_Duration'])/x['cell_ct']
+    if gi > 300:
+        return 60.0*x['Fix_Duration']
+    else:
+        return gi
